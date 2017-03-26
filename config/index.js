67c2966f7f9654ac95b1583506c1b13619bbecd5
chenpenghai <914266208@qq.com>
2017-03-26 16:52:02 +0800

    no message

--- config/index.js
+++ config/index.js
@@ -7,7 +7,7 @@ module.exports = {
     index: path.resolve(__dirname, '../products/index.html'),
     assetsRoot: path.resolve(__dirname, '../products'),
     assetsSubDirectory: 'static',
-    assetsPublicPath: '/',
+    assetsPublicPath: '/panda/products/',
     productionSourceMap: false,
     // Gzip off by default as many popular static hosts such as
     // Surge or Netlify already gzip all static assets for you.
