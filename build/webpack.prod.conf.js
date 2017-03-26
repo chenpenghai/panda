43bff4889f1451cd02fb711ea2681ecec1bc3741
chenpenghai <914266208@qq.com>
2017-03-26 16:39:03 +0800

    no message

--- build/webpack.prod.conf.js
+++ build/webpack.prod.conf.js
@@ -39,8 +39,8 @@ var webpackConfig = merge(baseWebpackConfig, {
     }),
     // extract css into its own file
     new ExtractTextPlugin({
-      // filename: utils.assetsPath('css/[name].[contenthash].css')
-      filename: '[name].[contenthash].css'
+      filename: utils.assetsPath('css/[name].[contenthash].css')
+      // filename: '[name].[contenthash].css'
     }),
     // Compress extracted CSS. We are using this plugin so that possible
     // duplicated CSS from different components can be deduped.
--- config/index.js
+++ config/index.js
@@ -7,7 +7,7 @@ module.exports = {
     index: path.resolve(__dirname, '../products/index.html'),
     assetsRoot: path.resolve(__dirname, '../products'),
     assetsSubDirectory: 'static',
-    assetsPublicPath: '',
+    assetsPublicPath: '/',
     productionSourceMap: false,
     // Gzip off by default as many popular static hosts such as
     // Surge or Netlify already gzip all static assets for you.
